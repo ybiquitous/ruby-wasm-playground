45eaa3ad460abc5166bc0ccb868ac86f94ff68aa
Masafumi Koba <473530+ybiquitous@users.noreply.github.com>
2022-09-08 13:36:11 +0900

    Use ESM and remove CJS

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # Ruby WASM Playground
 
-See the [sample code](index.cjs).
+See the [sample code](index.js).
 
 Run `npm it` (or `npm t`), then you should see the output below:
 
---
+++ index.js
@@ -0,0 +1,26 @@
+#!/usr/bin/env node --experimental-wasi-unstable-preview1
+
+import fs from "node:fs/promises";
+import { DefaultRubyVM } from "ruby-head-wasm-wasi/dist/node.cjs.js"; // TODO: `node.esm.js` is not working...
+
+const binary = await fs.readFile(
+  "./node_modules/ruby-head-wasm-wasi/dist/ruby.wasm"
+);
+const module = await WebAssembly.compile(binary);
+const { vm } = await DefaultRubyVM(module);
+
+console.log("");
+vm.printVersion();
+
+console.log("");
+console.log("-".repeat(30));
+
+let rubyCode = `
+    luckiness = ["Lucky", "Unlucky"].sample
+    puts "You are #{luckiness}"
+  `;
+const inputFile = process.argv[2];
+if (inputFile) {
+  rubyCode = await fs.readFile(inputFile);
+}
+vm.eval(rubyCode);
